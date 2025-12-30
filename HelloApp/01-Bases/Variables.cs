9f2c6b1a8050c2468d07d491de8f6f2c1888341e
Codecox-pl <patricklmo32@gmail.com>
2025-12-30 18:08:32 -0500

    Inicializando proyecto

---
+++ HelloApp/01-Bases/Variables.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+    static void Variables()
+    {
+        int number = 30;
+
+        string nombre = "Pedro";
+        bool esEstudiante = true;
+
+        Console.WriteLine($"Nombre: {nombre} tiene una edad {number} es estudiante {esEstudiante}");
+    }
+}
