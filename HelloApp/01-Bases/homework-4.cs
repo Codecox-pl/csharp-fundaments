6eb1f7c440272ddebe6e35945e9d5bbac86a99ae
Codecox-pl <patricklmo32@gmail.com>
2026-01-06 18:23:30 -0500

    Ejercicio 4 resuelto

--- HelloApp/01-Bases/homework-4.cs
+++ HelloApp/01-Bases/homework-4.cs
@@ -1,10 +1,29 @@
+using System.Globalization;
 partial class Program
 {
     static void DaysUntilNextBirthday()
     {
         Console.WriteLine("Ingrese su fecha de nacimiento (dd/mm/yyyy): ");
-        DateTime nac = DateTime.Parse(Console.ReadLine());
+        string inpt = Console.ReadLine()!;
+        DateTime nac = DateTime.ParseExact(inpt,"dd/MM/yyyy",CultureInfo.InvariantCulture);
 
-        Console.WriteLine(nac);
+        
+        int day = nac.Day;
+        int month = nac.Month;
+        int year = DateTime.Now.Year;
+        DateTime proxBirthay = new DateTime(year,month,day);
+
+        
+
+        if (month > DateTime.Now.Month)
+        {
+            proxBirthay.AddYears(1);
+        }
+
+        TimeSpan difference = proxBirthay - DateTime.Now ;
+        
+       System.Console.WriteLine(difference.ToString(@"dd\ d"));
+        
+
     }
 }
