d5137e3f8ff1108fdd62668e68b9c36c26340196
Codecox-pl <patricklmo32@gmail.com>
2026-01-17 00:09:31 -0500

    Dejar de rastrear archivos temporales

--- HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
+++ HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9620c451331fcb13eb2892d616c0bd5b3b71a5fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f997d8213a7566706441bc3690f8f57db4d0b981")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
