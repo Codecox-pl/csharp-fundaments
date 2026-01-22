2febdf2cbd616da44d0487206a26355bc6bef7c0
Codecox-pl <patricklmo32@gmail.com>
2026-01-22 10:58:46 -0500

    Dejar de rastrear carpetas bin y obj

--- HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
+++ HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e250a5a96ec09030ead08b3c7a5ab3eb13e473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d69bcbb8d0ca39e76d0fa8478a41026fa259d73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
